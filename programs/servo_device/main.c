8370edb37496e2e5b0b27f34ec3cc1d5d47c65dc
Carlos Pantelides <cpantel@github.com>
2021-10-03 11:06:49 -0300

    buttons control servo

--- programs/servo_device/main.c
+++ programs/servo_device/main.c
@@ -18,6 +18,8 @@
 
 #define BAUD_RATE 9600
 
+#define MAX_POS 10
+
 static void uart_putc(char c) {
     while (!(UART_STATUS & UART_STATUS_TX_READY));
     UART_DATA = c;
@@ -38,17 +40,52 @@ static inline uint32_t rdcycle(void) {
 
 
 int main() {
+    signed char position = 0;
+    char buttons;
     UART_BAUD = FREQ / BAUD_RATE; 
     LEDS = 0xAA;
 
+    char msg[15];
+    msg[0]='B';
+    msg[1]='u';
+    msg[2]='t';
+    msg[3]='t';
+    msg[4]='o';
+    msg[5]='n';
+    msg[6]='s';
+    msg[7]=':';
+    msg[8]='.';
+    msg[9]='.';
+    msg[10]='.';
+    msg[11]='.';
+    msg[12]='\r';
+    msg[13]='\n';
+    msg[14]='\0';
+
     for (;;) {
-        for (char i = 0; i<=10; ++i) {
-  //         uart_puts("Servo under control\r\n");
-           SERVO = i;
-           LEDS = ~LEDS;
-           uint32_t start = rdcycle();
-           while ((rdcycle() - start) <= FREQ);
+        SERVO = position;
+        buttons = ~BUTTONS;
+        LEDS = ~LEDS;
+
+        msg[8] =  ((buttons & 1 )      ) + '0';
+        msg[9] =  ((buttons & 2 ) >> 1 ) + '0';
+        msg[10] = ((buttons & 4 ) >> 2 ) + '0';
+        msg[11] = ((buttons & 8 ) >> 3 ) + '0';
+        uart_puts(msg);
+        if ((buttons & 8 ) >> 3 ) {
+            position = MAX_POS;
+        } else if (buttons & 1 ) {
+            position = 0;
+        } else if ((buttons & 2 ) >> 1 ) {
+            --position;
+            if (position < 0 ) position = 0;
+        } else if ((buttons & 4 ) >> 2 ) {
+            ++position;
+            if (position > MAX_POS) position = MAX_POS;
         }
+
+        uint32_t start = rdcycle();
+        while ((rdcycle() - start) <= FREQ);
     }
 
 }
