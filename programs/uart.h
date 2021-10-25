c7a916031d35026252652acedc270bb70559f979
Carlos Pantelides <cpantel@github.com>
2021-10-25 19:55:13 -0300

    refactored uart to its .h

---
+++ programs/uart.h
@@ -0,0 +1,17 @@
+#define UART_STATUS_TX_READY 0x1
+#define UART_STATUS_RX_READY 0x2
+
+#define BAUD_RATE 9600
+
+static void uart_putc(char c) {
+    while (!(UART_STATUS & UART_STATUS_TX_READY));
+    UART_DATA = c;
+}
+
+static void uart_puts(const char *str) {
+    char c;
+    while ((c = *str++)) {
+        uart_putc(c);
+    }
+}
+
